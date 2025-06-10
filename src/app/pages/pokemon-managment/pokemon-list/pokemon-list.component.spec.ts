806c3fd1eaaf474fff67b5ce9cb76ff35312a312
Luis Gustavo Rossim <luisrossim23@gmail.com>
2025-06-10 14:58:59 -0300

    fix(tests): jest pokemon-managment

--- src/app/pages/pokemon-managment/pokemon-list/pokemon-list.component.spec.ts
+++ src/app/pages/pokemon-managment/pokemon-list/pokemon-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonListComponent } from './pokemon-list.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('PokemonListComponent', () => {
   let component: PokemonListComponent;
@@ -8,7 +9,8 @@ describe('PokemonListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PokemonListComponent]
+      imports: [PokemonListComponent],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
